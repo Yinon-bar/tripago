a948b4f52cae0ccbee3fd7fd717e5f0e415ebd33
Yinon Bar <yinonbar1988@gmail.com>
2022-11-20 22:45:37 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import TripList from "./Components/TripList";
 
 function App() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    fetch(" http://localhost:3004/trips").then((resp) =>
+      resp.json().then((data) => setTrips(data))
+    );
+  }, []);
+
+  console.log(trips);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tripago</h1>
+      <TripList />
     </div>
   );
 }
---
+++ src/Components/TripList.js
@@ -0,0 +1,5 @@
+function TripList() {
+  return <div>TripList</div>;
+}
+
+export default TripList;
