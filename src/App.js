704adbb78f9d26e5639d26372aea0b6e9fc96424
Yinon Bar <yinonbar1988@gmail.com>
2022-11-20 23:00:58 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import TripList from "./Components/TripList";
 
 function App() {
-  const [trips, setTrips] = useState([]);
-
-  useEffect(() => {
-    fetch(" http://localhost:3004/trips").then((resp) =>
-      resp.json().then((data) => setTrips(data))
-    );
-  }, []);
-
-  console.log(trips);
-
   return (
     <div className="App">
-      <h1>Tripago</h1>
       <TripList />
     </div>
   );
--- src/Components/TripList.js
+++ src/Components/TripList.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import "./TripList.css";
+
 function TripList() {
-  return <div>TripList</div>;
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    fetch(" http://localhost:3004/trips").then((resp) =>
+      resp.json().then((data) => setTrips(data))
+    );
+  }, []);
+
+  console.log(trips);
+  return (
+    <div className="trip-list">
+      <h2>Trip list</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={Math.random()}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TripList;
