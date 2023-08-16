17c779b68d2f917a885f98ee69c3b6e470098ab5
Yinon Bar <yinonbar1988@gmail.com>
2023-08-16 23:09:12 +0300

    dummy text

---
+++ src/Components/TripList.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import useFetch from "../hooks/useFetch";
+import "./TripList.css";
+
+function TripList() {
+    // const [trips, setTrips] = useState([]);
+    const [url, setUrl] = useState("http://localhost:3000/trips");
+    const { data, isPending, error } = useFetch(url);
+    console.log(data);
+
+    return (
+        <div className="trip-list">
+            <h2>Trip list</h2>
+            {isPending && <h3>Loding Trips...</h3>}
+            {error && <h3>{error}</h3>}
+            <ul>
+                {data &&
+                    data.map((trip) => (
+                        <li key={Math.random()}>
+                            <h3>{trip.title}</h3>
+                            <p>{trip.price}</p>
+                        </li>
+                    ))}
+            </ul>
+            <div className="filters">
+                <button onClick={() => setUrl(url + "?loc=europe")}>
+                    European Trips
+                </button>
+                <button onClick={() => setUrl("http://localhost:3000/trips")}>
+                    All Trips
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default TripList;
